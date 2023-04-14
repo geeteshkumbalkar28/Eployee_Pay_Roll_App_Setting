119f973c3bd7b70bd82c2dad7859d69c60f02a8b
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-04-14 07:47:36 +0530

    Added : the Logging Levels, Logging to Console or File, Logging Patterns, etc based on this application running in Dev, Staging or Production

--- src/main/java/com/bridgelabz/employeepayroll/EmployeepayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeepayrollApplication.java
@@ -3,6 +3,10 @@ package com.bridgelabz.employeepayroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import javax.naming.Context;
 
 @SpringBootApplication
 @Slf4j
